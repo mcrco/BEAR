09d782dda2c15bd0190f02ba258e65209af5fb9f
mcrco <marco.yang.ca@gmail.com>
2024-07-03 16:17:01 -0700

    tadp mmaction wandb

--- benchmark/BEAR-Standard/mmaction/core/hooks/__init__.py
+++ benchmark/BEAR-Standard/mmaction/core/hooks/__init__.py
@@ -1,3 +1,4 @@
 from .output import OutputHook
+from .wandblogger import WandbHook
 
-__all__ = ['OutputHook']
+__all__ = ['OutputHook', 'WandbHook']
---
+++ benchmark/BEAR-Standard/mmaction/core/hooks/wandblogger.py
@@ -0,0 +1,22 @@
+import torch
+from mmcv.runner.hooks import HOOKS, Hook
+import wandb
+
+@HOOKS.register_module()
+class WandbHook(Hook):
+    """Check invalid loss hook.
+    This hook will regularly check whether the loss is valid
+    during training.
+    Args:
+        interval (int): Checking interval (every k iterations).
+            Default: 50.
+    """
+
+    def __init__(self, interval=50, name='mpii-tsh', project='tadvar', group='marco'):
+        self.interval = interval
+        wandb.init(name=name, project=project, group=group)
+
+    def after_val_epoch(self, runner):
+        log_dict = {'val_loss': runner.outputs['loss']}
+        log_dict.update(runner.outputs['log_vars'])
+        wandb.log(log_dict)
